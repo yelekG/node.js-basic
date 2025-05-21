52a3e5ed81febbd6a7d74ca71f69a357a344b331
yelekG <155242981+yelekG@users.noreply.github.com>
2025-05-21 21:28:10 +0300

    express eklendi

--- index.js
+++ index.js
@@ -2,33 +2,30 @@ import fs from "fs/promises";
 import http from "http";
 import path from "path";
 import url from "url";
+import express from "express";
 
+const app = express(); 
 const PORT = 8000;
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
-    let filePath;
-    if (req.url === "/") {
-        filePath = path.join(__dirname, "public", "index.html");
-    } else if (req.url === "/about") {
-       filePath = path.join(__dirname, "public", "about.html");
-    } else if (req.url === "/contact-me") {
-        filePath = path.join(__dirname, "public", "contact-me.html");
-    } else {
-        filePath = path.join(__dirname, "public", "404.html");
-    }
 
-    fs.readFile(filePath, "utf-8")
-        .then((data) => {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(data);
-        })
-        .catch((err) => {
-            res.writeHead(500, { "Content-Type": "text/plain"});
-            res.end("Server error", err);
-        })
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "contact-me.html"));
+});
+
+app.use((req,res) => {
+    res.status(404).sendFile(path.joiin(__dirname, "public", "404.html"));
+});
 
 server.listen(PORT, () => {
     console.log(`Server running at localhost${PORT}`);
