9edcff40365b0725999417634e716a6947903978
yelekG <155242981+yelekG@users.noreply.github.com>
2025-05-21 21:32:41 +0300

    yazım hataları

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import fs from "fs/promises";
-import http from "http";
 import path from "path";
-import url from "url";
+import { fileURLToPath } from "url";
 import express from "express";
 
 const app = express(); 
 const PORT = 8000;
 
-const __filename = url.fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
@@ -27,6 +25,6 @@ app.use((req,res) => {
     res.status(404).sendFile(path.joiin(__dirname, "public", "404.html"));
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running at localhost${PORT}`);
 })
